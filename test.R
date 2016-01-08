74bb754ffa5f367414e373d6c431c08eb267e496
TheLeopards <georgios.anastasiou@wur.nl>
2016-01-08 12:12:19 +0100

    test.R including script to write to file

--- test.R
+++ test.R
@@ -8,14 +8,12 @@ untar('data/LT51980241990098-SC20150107121947.tar.gz', exdir = "data/")
 list <- list.files('data/', pattern = glob2rx('*.tif'), full.names = TRUE)
 list
 
-turaStack <- stack(list[1:9])
+Landsat8 <- stack(list[1:9])
+Landsat8
 
-Landsat8 <- brick(list[1:9])
-warning()
-debug(stack)
-traceback()
+Landsat5 <- stack(list[10:24])
+Landsat5
 
+writeRaster(x=Landsat5, filename='data/Landsat5.grd', datatype='INT2S')
 
-
-
-
+writeRaster(x=Landsat8, filename='data/Landsat8.grd', datatype='INT2S')
